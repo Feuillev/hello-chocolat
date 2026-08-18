9a0c1f209caa1a81e04f8ed5389ba4b43fa6daf9
Feuillev <149873444+Feuillev@users.noreply.github.com>
2026-08-18 11:27:46 -0400

    Add files via upload

---
+++ peop.py
@@ -0,0 +1,22 @@
+    class obj():peop.py
+    x = 0
+    y = 0
+    coord = [x, y]
+    sprite1 = "perso_STAND"
+    sprite2 = "perso_MOVE"
+    sprite3 = "perso_MOVE2"
+    sprite4 = "perso_MOVE3"
+    sprite5 = "perso_MOVE4"
+    sprite6 = "perso_MOVE5"
+    animtion = [sprite1,sprite2,sprite3,sprite4,sprite5,sprite6, "\0"]
+        def deplcm():
+            disp = ""
+            my_poeple = new(obj)
+            my_poeple.x  = KEY_MOUSE()
+            my_poeple.y  = KEY_MOUSE()
+            k = 0
+            my_poeple.coord[x, y]
+            while animation[k] != "\0":
+                disp = animation[k]
+                k = k + 1
+        
