48b4c934bd9aa562bc58cc7c9df554c738238838
Feuillev <149873444+Feuillev@users.noreply.github.com>
2026-08-18 02:11:43 -0400

    Add files via upload
    my commit is about drop some change about the way to make in order things

---
+++ trous.py
@@ -0,0 +1,24 @@
+def boite_obj():
+    int i = 0
+    int j = 0
+    bt = [pen, eraz, pensl, stab, mn, fir]
+    pen = 5
+    eraz = 2
+    pensl = 3
+    stab = 4
+    mn = 18
+    fir = 1
+    bt_2 = [,]
+    bt_3 = [,]
+    while(bt_[i] != 'null'):
+        if i <= 1:
+            bt_2.append() = bt[i]
+            i = i + 1
+        if i <= 3
+            bt_3.append() = bt[i]
+            i = i + 1
+        if i <= 5:
+            bt_4.append() = bt[i]
+            i = i + 1
+    return()
+    
