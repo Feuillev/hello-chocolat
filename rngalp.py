044eeba289d6d41cfcba5be7701e3d228e6abf67
Feuillev <149873444+Feuillev@users.noreply.github.com>
2026-08-19 12:42:17 -0400

    Add files via upload

---
+++ rngalp.py
@@ -0,0 +1,17 @@
+def rng():
+    i =0
+    j = 0
+    word_to_range = []
+    while i != 25:
+        alph = [a,b,c,d,e,f,g,h,i,j,k,l,m,o,p,q,r,s,t,u,v,w,x,y,z]
+        alphh[i] = ['alph[i]']
+        i = i + 1
+    i = 0    
+    while i !=25:
+        if alph[i] != word_to_range[(1)[j]]:
+            i = i + 1
+        else
+            alpha[i].append() = word_to_range[i]
+            i = 0
+            j = j + 1
+            
