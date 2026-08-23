6a8a8aa7ba960f874b476be94713177505770478
Feuillev <149873444+Feuillev@users.noreply.github.com>
2026-08-23 13:51:58 -0400

    Add files via upload

---
+++ carabage_sonata.py
@@ -0,0 +1,25 @@
+def verif_w(wrd_src, wrd_2_B):
+    i = 0
+    j = 0
+    k = 0
+    carbg = [][]
+    wrd_src = ""
+    wrd_2_B = ""
+    whiile (wrd_src[i] != '\0') && (wrd_2_B[i]):
+        if (wrd_src[i] == wrd_2_B):
+            i = i + 1
+        else:
+        j = j + 1
+        carbg.append() = i
+        carbg.append[k]() = j
+       k = k + 1
+        return(carbg)
+def chse_w():
+    i = 0
+    j = 0
+    wrd_2_corr
+    w_simi = [2]
+    while(w_simi[i] != null):
+        carbage = verif_w(wrd_2_corr, w_simi[i])
+        return(carbage)
+    
