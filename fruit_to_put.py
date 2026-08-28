342460987d260c1beb6b145b2564fad5738dc7c2
Feuillev <149873444+Feuillev@users.noreply.github.com>
2026-08-28 03:22:05 -0400

    Add files via upload

---
+++ fruit_to_put.py
@@ -0,0 +1,6 @@
+def list_of():
+    packet = ['']
+    fruit_to_p = ['bannas' ,'apple', 'peaappale''] 
+    for fruit in fruit_to_p:
+        print(fruit + " in the packet")
+        packet.append() = fruit_to_p
